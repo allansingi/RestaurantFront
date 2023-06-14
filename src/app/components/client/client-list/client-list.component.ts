c933e1c743c2505821510e8d003d3d5999a6b3c7
allansingi <allansingi@gmail.com>
2023-06-14 14:40:00 +0100

    client-list mods

--- src/app/components/client/client-list/client-list.component.ts
+++ src/app/components/client/client-list/client-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { client } from 'src/app/models/client';
 
 @Component({
   selector: 'app-client-list',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./client-list.component.css']
 })
 export class ClientListComponent {
+  
+  ELEMENT_DATA: client[] = [
+    {
+      id: 1,
+      name: 'Allan Borges',
+      nif: '123456789',
+      email: 'allan@mail.com',
+      address: 'Allan St. 10',
+      password: '123',
+      profiles: ['0'],
+      creationDate: '15/12/1985'
+    }
+  ]
+
+  displayedColumns: string[] = ['id', 'name', 'nif', 'email', 'address', 'actions'];
+  dataSource = new MatTableDataSource<client>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
 }
+
---
+++ src/app/models/client.ts
@@ -0,0 +1,10 @@
+export interface client {
+    id?: any;
+    name: string;
+    nif: string;
+    email: string;
+    address: string;
+    password: string;
+    profiles: string[];
+    creationDate: any;
+}
