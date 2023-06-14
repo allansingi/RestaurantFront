7a5a6875d6be347b93c1d2f9105b82a1dacca892
allansingi <allansingi@gmail.com>
2023-06-14 11:08:31 +0100

    home component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,11 +29,13 @@ import { MatCardModule } from '@angular/material/card';
 
 //Project Components
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
