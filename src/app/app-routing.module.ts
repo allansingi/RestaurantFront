9272a59e4e890e649ea0a0ef159c99379d8c8567
allansingi <allansingi@gmail.com>
2023-06-14 12:33:29 +0100

    route for client-list created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { ClientListComponent } from './components/client/client-list/client-list.component';
 
 const routes: Routes = [
   {
     path: '', component: NavComponent, children: [
-      {path: 'home', component: HomeComponent}
+      {path: 'home', component: HomeComponent},
+      {path: 'clients', component: ClientListComponent}
     ]
   }
 ];
