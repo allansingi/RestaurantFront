c8b87efab5bf9c1ed6bca936a9b3fa4cb6f0c792
allansingi <allansingi@gmail.com>
2023-06-14 12:23:58 +0100

    client-list component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,13 +31,15 @@ import { MatCardModule } from '@angular/material/card';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ClientListComponent } from './components/client/client-list/client-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    ClientListComponent
   ],
   imports: [
     BrowserModule,
