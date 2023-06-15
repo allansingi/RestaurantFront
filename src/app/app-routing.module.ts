9b983923a40dba2c775b6b37c3112ac2b0dd29e8
allansingi <allansingi@gmail.com>
2023-06-15 17:10:18 +0100

    route for client-create added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,16 @@ import { HomeComponent } from './components/home/home.component';
 import { ClientListComponent } from './components/client/client-list/client-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
+import { ClientCreateComponent } from './components/client/client-create/client-create.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
     path: '', component: NavComponent, canActivate: [AuthGuard], children: [
       {path: 'home', component: HomeComponent},
-      {path: 'clients', component: ClientListComponent}
+
+      {path: 'clients', component: ClientListComponent},
+      {path: 'clients/create', component: ClientCreateComponent}
     ]
   }
 ];
